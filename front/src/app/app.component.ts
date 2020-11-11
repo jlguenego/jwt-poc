6ab4c825bb854fad497a6a0f3f98db10083f6026
Backuper <please@enjoy-jlg-backup.com>
2020-11-11 15:45:47 +0100

    added component login

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+import { Secret } from './interfaces/secret';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'front';
+  isConnected = false;
+
+  f = new FormGroup({
+    login: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  secret: Secret = {};
+
+  constructor(private http: HttpClient) {}
+
+  submit(): void {
+    console.log('submit');
+  }
+
+  getSecret(): void {
+    this.http.get<Secret>('http://localhost:3000/ws/now').subscribe({
+      next: (secret) => {
+        console.log('secret: ', secret);
+        this.secret = secret;
+      },
+      error: (err) => {
+        console.log('err: ', err);
+      },
+    });
+  }
 }
---
+++ front/src/app/interfaces/secret.ts
@@ -0,0 +1,3 @@
+export interface Secret {
+  [key: string]: string;
+}
