21572aca0ac9b3b1ee187ba1c1ba64e0506a5cdd
Backuper <please@enjoy-jlg-backup.com>
2020-11-13 12:27:56 +0100

    added nyc

--- back/test/first.spec.ts
+++ back/test/first.spec.ts
@@ -4,4 +4,10 @@ describe("NETAPI Unit Test", function () {
   it("test for fun", function () {
     assert.ok("super");
   });
+  it("autre test", function () {
+    assert.ok("super");
+  });
+  it("last test", function () {
+    assert.ok("super");
+  });
 });
