b7fbe7818e147cc76cbeef270d2dc774a6d857d0
Backuper <please@enjoy-jlg-backup.com>
2020-11-13 12:23:26 +0100

    added mocha

--- back/.eslintrc.js
+++ back/.eslintrc.js
@@ -11,4 +11,7 @@ module.exports = {
   rules: {
     eqeqeq: ["error", "smart"],
   },
+  standard: {
+    env: ["mocha"],
+  },
 };
---
+++ back/test/first.spec.ts
@@ -0,0 +1,7 @@
+import { strict as assert } from "assert";
+
+describe("NETAPI Unit Test", function () {
+  it("test for fun", function () {
+    assert.ok("super");
+  });
+});
